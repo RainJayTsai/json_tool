58cbae6fc270266c7a89f71edd69958cb055e683
rainjay <RainJay0117@gmail.com>
2017-05-13 20:52:50 +0800

    init

---
+++ src/components/FileInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <input type="file" name="points" @change="onFileChange"/>
+        <p>You Select File is <input type="text" v-model="file_path"></p>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                file_path: ""
+            }
+        },
+        methods:{
+            onFileChange(e){
+                this.file_path = e.target.value
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
